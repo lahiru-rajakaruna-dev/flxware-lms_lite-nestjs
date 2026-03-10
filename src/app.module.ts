92cca5ae2fc336f4b843c276a512a1d0787b2da5
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-10 22:55:31 +0530

    feat(app module): add middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from './cache/cache.module';
+import { LoggerModule } from './logger/logger.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { MiddlewareModule } from './middleware/middleware.module';
+import { SessionMiddleware } from './middleware/session.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule,
+    LoggerModule,
+    MiddlewareModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(AuthMiddleware, SessionMiddleware, LoggerMiddleware)
+      .exclude('/health')
+      .forRoutes('*');
+  }
+}
