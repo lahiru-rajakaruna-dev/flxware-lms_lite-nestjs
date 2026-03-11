b55a20a6e9bd1ac01bac9cb11f7424c3ac9fc31d
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 09:35:50 +0530

    refactor(cache module): updated cache value type

--- src/cache/interface.cache.ts
+++ src/cache/interface.cache.ts
@@ -3,8 +3,8 @@ export interface ICacheService {
   set(key: string, value: Omit<TCacheValue, 'undefined'>): boolean;
 }
 
-export type TCacheValue =
-  | string
-  | number
-  | Record<string, string | number>
-  | undefined;
+export type TCacheValue = string | number | TObject | undefined;
+
+export type TObject = {
+  [k: string]: string | number | Date | TObject;
+};
