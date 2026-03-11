9ff162448a27b1e9e46141eef1ef87cf3206bb44
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 10:54:08 +0530

    refactor(middleware module): add missing modules

--- src/middleware/middleware.module.ts
+++ src/middleware/middleware.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '../cache/cache.module';
 import { LoggerModule } from '../logger/logger.module';
+import { SessionModule } from '../session/session.module';
 import { AuthMiddleware } from './auth.middleware';
 import { LoggerMiddleware } from './logger.middleware';
 import { SessionMiddleware } from './session.middleware';
 
 @Module({
-  imports: [CacheModule, LoggerModule],
+  imports: [CacheModule, LoggerModule, SessionModule],
   providers: [AuthMiddleware, SessionMiddleware, LoggerMiddleware],
   exports: [AuthMiddleware, SessionMiddleware, LoggerMiddleware],
 })
