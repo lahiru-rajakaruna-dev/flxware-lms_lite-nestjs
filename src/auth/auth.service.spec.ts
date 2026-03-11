3f367f37310eb0129755758fe4c875c7dc7dfe88
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 10:50:50 +0530

    refactor(auth service test): add missing modules

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,8 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import path from 'node:path';
+import { RepositoryModule } from '../repository/repository.module';
+import { SessionModule } from '../session/session.module';
 import { AuthService } from './auth.service';
 
 jest.mock('uuid', () => ({ _uuid: () => Uint8Array.from('mocked') }));
@@ -9,7 +12,14 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: path.resolve(process.cwd(), '.env'),
+          isGlobal: true,
+        }),
+        SessionModule,
+        RepositoryModule,
+      ],
       providers: [AuthService],
     }).compile();
 
