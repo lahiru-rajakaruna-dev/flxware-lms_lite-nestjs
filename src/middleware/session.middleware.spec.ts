e914321c6efedc9b0a76718316605430c3e436e1
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 10:54:36 +0530

    refactor(session middleware test): mocked uuid

--- src/middleware/session.middleware.spec.ts
+++ src/middleware/session.middleware.spec.ts
@@ -3,6 +3,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import path from 'node:path';
 import { CacheModule } from '../cache/cache.module';
 import { SessionMiddleware } from './session.middleware';
+import { v4 as _uuid } from 'uuid';
+
+jest.mock('uuid', () => {
+  return { _uuid: () => Uint8Array.from('mocked') };
+});
 
 describe('SessionMiddleware', () => {
   let service: SessionMiddleware;
