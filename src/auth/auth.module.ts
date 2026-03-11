9efa36a6bd16c87841feebfbb10ddeaed4330a55
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 10:53:00 +0530

    refactor(auth module): add missing modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RepositoryModule } from '../repository/repository.module';
+import { SessionModule } from '../session/session.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [RepositoryModule, SessionModule],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
