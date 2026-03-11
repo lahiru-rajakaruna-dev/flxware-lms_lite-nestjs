74ac6af3f70daca37b19badf06b808cce8789b5f
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-11 09:44:44 +0530

    feat(middleware module): add jwt decoding

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,20 +1,39 @@
 import { NestMiddleware } from '@nestjs/common';
-import { global } from '../../global';
-import NestRequest = global.NestRequest;
+import { ConfigService } from '@nestjs/config';
+import { e } from '../../global';
+import NestRequest = e.NestRequest;
+import * as JWT from 'jsonwebtoken';
+import { AuthenticationError } from '../auth/auth.service';
 
 export class AuthMiddleware implements NestMiddleware {
-  constructor() {}
+  private readonly configService: ConfigService;
+
+  constructor(configService: ConfigService) {
+    this.configService = configService;
+  }
 
   public use(
     req: NestRequest,
     res: Response,
     next: (error?: any) => void,
   ): any {
-    const authToken = req.cookies['authorization'];
+    const authToken = req.cookies['token'];
 
     if (!authToken) {
       throw new AuthError('Unauthenticated');
     }
+
+    const JWT_SECRET_KEY = this.configService.get('JWT_SECRET_KEY') as string;
+
+    const sessionKey = JWT.verify(authToken, JWT_SECRET_KEY, {
+      complete: true,
+    }).payload;
+
+    if (!sessionKey) {
+      throw new AuthenticationError('Invalid token');
+    }
+
+    req.sessionKey = sessionKey as string;
 
     return next();
   }
